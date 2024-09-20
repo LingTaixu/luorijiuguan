5365de0c23f34ca9b43dea30de1c587c8c40f11d
陈碎碎 <taixuling@gmail.com>
2024-09-20 16:11:26 +0800

    feat:  vercel  Web Analytics

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,11 +1,12 @@
 <template>
   <div class="home">
     <slot />
+    <SpeedInsights />
   </div>
 </template>
 
 <script setup lang="ts">
-
+import { SpeedInsights } from "@vercel/speed-insights/nuxt"
 </script>
 
 <style scoped>
